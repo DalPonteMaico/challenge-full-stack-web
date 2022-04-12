ae4f07a445c6a3b66ecd8fc7c5062505dc0a9181
Dalponte <maico.dalponte@codengage.com>
2022-04-12 06:11:45 -0300

    feat(front): add api call to create student

--- front/src/views/CreateStudentPage.vue
+++ front/src/views/CreateStudentPage.vue
@@ -5,48 +5,50 @@
     </v-app-bar>
 
     <v-container>
-        <form @submit.prevent="submit">
-            <v-text-field
-                v-model="name"
-                label="Nome"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="email"
-                label="E-mail"
-                required
-            ></v-text-field>
-            <v-text-field
-                v-model="registration"
-                label="RA"
-                readonly
-            ></v-text-field>
-            <v-text-field
-                v-model="socialIdNumber"
-                label="CPF"
-                required
-            ></v-text-field>
-          <div class="d-flex justify-end">
+      <form @submit.prevent="submit">
+        <v-text-field
+            v-model="name"
+            label="Nome"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="email"
+            label="E-mail"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="registration"
+            label="RA"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="socialIdNumber"
+            label="CPF"
+            required
+        ></v-text-field>
+        <div class="d-flex justify-end">
 
-            <v-btn class="mr-4 secondary" @click="clear">
-              Cancelar
-            </v-btn>
+          <v-btn class="mr-4 secondary" @click="clear">
+            Cancelar
+          </v-btn>
 
-            <v-btn
-                class=" primary"
-                type="submit"
-            >
-              Salvar
-            </v-btn>
+          <v-btn
+              class=" primary"
+              type="submit"
+          >
+            Salvar
+          </v-btn>
 
-          </div>
-        </form>
+        </div>
+      </form>
 
     </v-container>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'CreateStudents',
   data: () => ({
@@ -58,12 +60,25 @@ export default {
 
   methods: {
     submit() {
-      console.log({
-        name: this.name,
-        email: this.email,
-        registration: this.registration,
-        socialIdNumber: this.socialIdNumber,
-      })
+      const payload = {
+        student: {
+
+          name: this.name,
+          email: this.email,
+          registration: this.registration,
+          socialIdNumber: this.socialIdNumber,
+        }
+      }
+
+      axios
+          .post('http://localhost:3001/students', payload)
+          .then(response => {
+            console.log(response.data)
+            if (response.status === 201)
+              this.$router.push('/students')
+            else
+              console.error('Error', response)
+          })
     },
     clear() {
       this.name = ''
